02ca3051e47ef414351c18aa5b5bdbcc04ecbf2e
Jared  E <jared.epps322@gmail.com>
2020-05-22 21:49:32 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,23 +4,6 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def initialize(:name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width)
-@name = name 
-@birthday = birthday 
-@hair_color = hair color 
-@eye_color = eye color 
-@height = height 
-@weight = weight 
-@handed = handed 
-@complexion = complextion 
-@t_shirt_size = t shirt size 
-@wrist_size = wrist size 
-@glove_size = glove size 
-@pant_width = pant length 
-@pant_width = pant width 
-end 
-  
+  def initialize(
   
 end
